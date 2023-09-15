0b6ce1d0699922b018ac4c7c5fb3345c523c51af
남홍식(hongsik_nam) <hs.nam@purple.io>
2023-09-15 09:12:12 +0900

    redis 동시성 테스트 작성

--- src/redis-test.ts
+++ src/redis-test.ts
@@ -1,33 +1,38 @@
-import redis from 'redis';
-import redisLock from 'redis-lock';
+import Redis from 'ioredis';
+import Redlock from 'redlock';
 
+const redis = new Redis();
+const redlock = new Redlock([redis],  {
+    driftFactor: 0.01, // clock drift를 보상하기 위해 driftTime 지정에 사용되는 요소, 해당 값과 아래 ttl값을 곱하여 사용.
+    retryCount: 10, // 에러 전까지 재시도 최대 횟수
+    retryDelay: 200, // 각 시도간의 간격
+    retryJitter: 200, // 재시도시 더해지는 되는 쵀대 시간(ms)
+    automaticExtensionThreshold: 500, // lock 연장 전에 남아야 하는 최소 시간(ms)
+});
 
 
 async function performConcurrencyTest() {
-    const client = redis.createClient();
-    const lock = redisLock(client);
-    await client.connect();
+    let lock = await redlock.lock(["a"], 5000);
     try {
-        const done = await lock('device')
-        // Redis에서 사용자의 잔액을 가져옴
-        const deviceId = await client.get(`device_id`);
+        const deviceId = await redis.get(`device_id`);
 
         if (deviceId === null) {
             console.log(`device_id id null...`);
             // 사용자가 없으면 생성하고 초기 잔액을 설정
-            await client.set(`device_id`, 1);
+            await redis.set(`device_id`, 1);
         } else {
             // 잠시 대기 (동시성 테스트를 위해)
             await new Promise((resolve) => setTimeout(resolve, 2000)); // 2초 동안 대기
             // 사용자가 있으면 잔액을 업데이트
             const newDeviceId = parseInt(deviceId) + 1;
-            await client.set(`device_id`, newDeviceId);
+            await redis.set(`device_id`, newDeviceId);
             console.log(`${new Date()} - new DeviceId = ${newDeviceId}`);
         }
-        await done()
+        await lock.unlock();
     } catch (error) {
         console.error("Error:", error);
     }
+    console.log('release the lock');
 }
 
 
